c277cb7f27cf1c67ffdd8706a94231b5b0a95e48
Matthew Hamilton <matthew.hamilton2@unibo.it>
2025-03-26 13:23:44 +0000

    add esp-idf tests

---
+++ src/Diagnostics/Diagnostics.ino
@@ -0,0 +1,48 @@
+/*
+  Component Diagnostics
+
+    Confirm the connection of every component and print to serial any errors.
+
+  | Connection      | Pin   |
+  | --------------- | ----- |
+  | NC              | D13   |
+  | EEPROM VCC      | 3.3v  |
+  | AREF POT Switch | AREF  |
+  | PCB 0 QRE1113   | A0    |
+  | PCB 1 QRE1113   | A1    |
+  | PCB 2 QRE1113   | A2    |
+  | PCB 3 QRE1113   | A3    |
+  | PCB 4 QRE1113   | A4    |
+  | PCB 5 QRE1113   | A6    |
+  | PCB 6 QRE1113   | A7    |
+  | NC              | 5v    |
+  | NC              | RESET |
+  | GND             | GND   |
+  | PSU `+`         | VIN   |
+
+  | Connection      | Pin     |
+  | --------------- | ------- |
+  | Rotary CLK      | D12     |
+  | Rotary DATA     | D11     |
+  | Rotary Switch   | D10     |
+  | LED Data        | D9      |
+  | Mux C           | D8      |
+  | Mux B           | D7      |
+  | Mux A           | D6      |
+  | EEPROM SPI CLK  | D5      |
+  | EEPROM SPI MISO | D4      |
+  | EEPROM SPI MOSI | D3      |
+  | EEPROM SPI CS   | D2      |
+  | NC              | RX / D1 |
+  | NC              | TX / D0 |
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
